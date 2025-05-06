95d1b4bf7b55b43e994a41e507ac8e6a294210ea
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-05-06 09:31:07 +0530

    session-01 practicals

---
+++ Session-01/P2.java
@@ -0,0 +1,8 @@
+class P2 {
+    public static void main(String[] args) {
+        System.out.println("The first argument is " + args[0]);
+        System.out.println("The second argument is " + args[1]);
+        System.out.println("The third argument is " + args[2]);
+      
+    }
+}
