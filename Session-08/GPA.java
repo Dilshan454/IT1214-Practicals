2cb59b47a5aad341d9e35986ed14e6e493c163d8
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-05-26 00:24:31 +0530

    fix errors

--- Session-08/GPA.java
+++ Session-08/GPA.java
@@ -5,45 +5,46 @@ class GPA{
 	float gradePoint(String grade){
 		float gp=0;//grade point value
 		
-		/*if(grade=="A+" || grade=="A")
-		{
-			gp=4;
-		}
-		else if(grade=="A-")
-		{
-			gp=3.7f;
-		}*/
-		
-		
 		
 		switch(grade){
-			case"A+":
-			gp=4;
-			break;
-			case"A":
-			gp=4;
-			break;
-			case"A-":
-			gp=3.7;
-			break;
-			case"B+":
-			gp=3.3;
-			break;
-			case"B":
-			gp=3;
-			break;
-			case"B-":
-			gp=2.7;
-			break;
-			case"C+":
-			gp=2.3;
-			break;
-			case"C":
-			gp=2;
-			break;
-			case"C-":
-			gp=1.7;
-			break;}
+			case "A+":
+				gp=4;
+				break;
+			case "A":
+				gp=4;
+				break;
+			case "A-":
+				gp=3.7f;
+				break;
+			case "B+":
+				gp=3.3f;
+				break;
+			case "B":
+				gp=3;
+				break;
+			case "B-":
+				gp=2.7f;
+				break;
+			case "C+":
+				gp=2.3f;
+				break;
+			case "C":
+				gp=2;
+				break;
+			case "C-":
+				gp=1.7f;
+				break;
+			case "D+":
+				gp=1.3f;
+				break;
+			case "D":
+				gp=1;
+				break;
+				
+			default:
+				gp=0;
+				break;
+			}
 			
 			return gp;
 	}
@@ -51,22 +52,68 @@ class GPA{
 }
 
 class Demo{
+	static  Scanner sc=new Scanner(System.in);
+	static GPA g=new GPA();
+	static String subjects[]={"IT1113","IT1122","IT1134","IT1144","IT1152"};
+	static int credits[]={3,2,4,4,2};
+	
+	static String[] grades=new String[5];
+	static float[] gps=new float[5];
+	
+	static float getPoint(String subjectCode,int credit,int index){
+		System.out.print("Enter the Grade for "+subjectCode+": ");
+		grades[index]=sc.nextLine();
+		System.out.println("Grade point value of the grade "+grades[index]+" is "+g.gradePoint(grades[index]));
+		System.out.println("Overall Grade point is "+g.gradePoint(grades[index])*credit);
+		return g.gradePoint(grades[index])*credit;
+		
+	
+
+	}
+	
 	
 	public static void main(String[] args){
-		String name,grade;
-		float ogpa,point;
+		String name/*,grade*/;
+		float ogp=0/*,point=0*/;
 		
-		Scanner sc=new Scanner(System.in);
-		GPA g=new GPA();
+		/*Scanner sc=new Scanner(System.in);
+		GPA g=new GPA();*/
 		
 		System.out.println("Enter your Name: ");
 		name=sc.nextLine();
 		System.out.println("Hi "+name);
 		
-		System.out.println("Enter the grade for IT1113: ");
+		for(int i=0;i<5;i++){
+			ogp+=getPoint(subjects[i],credits[i],i);
+		}
+		
+		
+		
+		/*System.out.println("Enter the grade for IT1113: ");
 		grade=sc.nextLine();
 		point=g.gradePoint(grade);
-		ogp=ogp+point*3;
+		ogp+=point*3;
 		System.out.println("Grade point value of the grade "+grade+" is "+point);
+		System.out.println("Overall Grade point is "+ogp);
+		
+		System.out.println("Enter the grade for IT1122: ");
+		grade=sc.nextLine();
+		point=g.gradePoint(grade);
+		ogp+=point*2;
+		System.out.println("Grade point value of the grade "+grade+" is "+point);
+		System.out.println("Overall Grade point is "+ogp);*/
+		
+		/*ogp+=getPoint("IT1113",3);//read the Grade of IT1113,Get the Point value,return the point*credit.
+		ogp+=getPoint("IT1122",2);
+		ogp+=getPoint("IT1134",4);
+		ogp+=getPoint("IT1144",4);
+		ogp+=getPoint("IT1152",2);*/
+		
+		System.out.println("Final GPA is for current semester is : "+ogp/15);
+	
+		for(int i=0;i<5;i++){
+			System.out.println("Subject "+subjects[i]+" Credit "+credits[i]+" Grade "+grades[i]+" Grade Point "+gps[i]);
+		}
+	
 	}
 }
