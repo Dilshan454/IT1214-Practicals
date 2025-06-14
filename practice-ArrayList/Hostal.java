938e476ad8a9c8eeadcb68b5d60966c9d52c076a
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-06-14 22:56:44 +0530

    hostel system

---
+++ practice-ArrayList/Hostal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Room{
+    int[] age=new int[4];
+    String[] name=new String[4];
+
+    int roomNo;
+}
+
+public class Hostal{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Room[] room =new Room[5];
+
+        Room r1=new Room();
+        System.out.println("Enter students names and age");
+        sc.nextLine();
+    }
+}
