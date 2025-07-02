bc5d70a4346f13130eb4c804df99f624421db8e6
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-07-02 22:39:26 +0530

    model paper try

---
+++ modelpaper2/ArrayOperations.java
@@ -0,0 +1,11 @@
+public class ArrayOperations{
+    public static void main(String[] args){
+        double[] arr={12.5, 45.8, 23.1, 67.9, 34.2, 89.7, 56.3, 78.4, 91.6, 25.8};
+
+
+    }
+
+    double findMax(dou[] ar){
+        
+    }
+}
