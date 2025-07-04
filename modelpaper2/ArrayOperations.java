0aaa58cd270d8e3c71c6588e6e7d8a896f1f3b4d
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-07-04 23:11:01 +0530

    model question

--- modelpaper2/ArrayOperations.java
+++ modelpaper2/ArrayOperations.java
@@ -2,10 +2,43 @@ public class ArrayOperations{
     public static void main(String[] args){
         double[] arr={12.5, 45.8, 23.1, 67.9, 34.2, 89.7, 56.3, 78.4, 91.6, 25.8};
 
+        double max=findMax(arr);
+        double min=findMin(arr);
+        double avg=findAvg(arr);
+
+        System.out.println("largest number: "+max);
+        System.out.println("minimum number: "+min);
+        System.out.println("average: "+avg);
 
     }
 
-    double findMax(dou[] ar){
+    public static double findMax(double[] ar){
+        double max=ar[0];
+        for(double num:ar){
+            if(num>max){
+                max=num;
+            }
+        }
+        return max;
         
+    }
+
+    public static double findMin(double[] ar){
+        double min=ar[0];
+        for(double num:ar){
+            if(num<min){
+                min=num;
+            }
+        
+        }
+        return min;
+    }
+
+    public static double findAvg(double[] ar){
+        double sum=0;
+        for(double num:ar){
+            sum+=num;
+        }
+        return sum/ar.length;
     }
 }
